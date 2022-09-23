8cf25a86702b4ca4c01f95da58dcc84f06b037f0
vika29061995q <6270655@gmail.com>
2022-09-23 18:41:06 +0300

    homework3 and homework4

---
+++ hw3/task1.ts
@@ -0,0 +1,169 @@
+//task1
+const x = 20;
+const y = 58;
+const z = 42;
+const task1: number = x + y + z;
+console.log("task1 = " + task1);
+
+//task2
+const secinmin = 60;
+const mininhour = 60;
+const hourinat = 24;
+const daysinyear = 356;
+const myAge = 27;
+const myAgeInSeconds = myAge * daysinyear * hourinat * mininhour * secinmin;
+console.log("task2 = " + myAgeInSeconds);
+
+//task3
+const count = 42;
+const userName = "42";
+const countStr1 = String(count);
+const countStr2 = `${count}`;
+const userNameNum1 = +userName;
+const userNameNum2 = Number(userName);
+console.log(
+    "task3 = " + typeof countStr1 + " " + typeof countStr2 + " " + typeof userNameNum1 + " " + typeof userNameNum2,
+);
+
+//task4
+const a = 1;
+const b = 2;
+const c = "белых медведей";
+console.log("task4 = " + a + b + " " + c);
+
+//task5
+const word1 = "доступ";
+const word2 = "морпех";
+const word3 = "наледь";
+const word4 = "попрек";
+const word5 = "рубило";
+
+const length1 = word1.length;
+const length2 = word2.length;
+const length3 = word3.length;
+const length4 = word4.length;
+const length5 = word5.length;
+
+const lengthWords = length1 + length2 + length3 + length4 + length5;
+console.log("task5 = " + lengthWords);
+
+//task6
+const variable1 = 123;
+const variable2 = "home";
+const variable3 = false;
+console.log("task6 = " + "Variable: " + variable1 + " have type: " + typeof variable1);
+console.log("task6 = " + "Variable: " + variable2 + " have type: " + typeof variable2);
+console.log("task6 = " + "Variable: " + variable3 + " have type: " + typeof variable3);
+
+//task7
+const var1 = "true";
+const var2 = false;
+const var3 = 17;
+const var4 = undefined;
+const var5 = null;
+console.log("task7 = " + typeof var1 + " " + typeof var2 + " " + typeof var3 + " " + typeof var4 + " " + typeof var5);
+
+//task8
+const height = 15;
+const width = 20;
+
+if (height > width) {
+    console.log("task8 = " + height);
+} else {
+    console.log("task8 = " + width);
+}
+
+//task9
+console.log("task9");
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log(i);
+    }
+}
+
+//task10
+const key = true;
+const documents = true;
+const pen = true;
+const apple = false;
+const orange = false;
+
+if (key && documents && pen) {
+    if (apple || orange) {
+        const shouldGoToWork = "you can go to work";
+        console.log("task10 " + shouldGoToWork);
+    } else {
+        const shouldGoToWork = "you can't go to work";
+        console.log("task10 " + shouldGoToWork);
+    }
+}
+
+//task11
+console.log("task11");
+const task11 = 99;
+
+if (task11 % 5 === 0 && task11 % 3 === 0) {
+    console.log("FizBuz");
+} else if (task11 % 5 === 0) {
+    console.log("Fiz");
+} else if (task11 % 3 === 0) {
+    console.log("Buz");
+}
+
+//task12
+console.log("task12");
+const age = 16;
+if (age >= 18) {
+    console.log(`Попей пивка`);
+} else if (16 <= age && age < 18) {
+    console.log(`Можешь выкурить сигаретку, только маме не говори`);
+    console.log(`Пей колу`);
+} else {
+    console.log(`Пей колу`);
+}
+
+//task13
+console.log(`task13`);
+const south = "юг";
+const north = "север";
+const west = "запад";
+const east = "восток";
+
+const myChoice = east;
+
+switch (myChoice) {
+    case south:
+        console.log(myChoice + ` на юг пойдешь счастье найдешь`);
+        break;
+    case north:
+        console.log(myChoice + ` на север пойдешь много денег найдешь`);
+        break;
+    case west:
+        console.log(myChoice + ` на запад пойдешь верного друга найдешь`);
+        break;
+    case east:
+        console.log(myChoice + ` на восток пойдешь разработчиком станешь`);
+        break;
+}
+
+//Advanced task1
+let aa = 4;
+let bb = 3;
+
+aa += bb;
+bb = aa - bb;
+aa -= bb;
+console.log(`Advanced task1 a=` + aa + " b=" + bb);
+
+//Advanced task2
+console.log(`Advanced task2`);
+const char = "#";
+const loopCount = 7;
+
+for (let i = 1; i < loopCount; i++) {
+    let charsave = "";
+    for (let y = 0; y < i; y++) {
+        charsave += char;
+    }
+    console.log(charsave);
+}
---
+++ hw4/homework4.ts
@@ -0,0 +1,123 @@
+//task 1
+console.log("task 1");
+const MyName = "Victoria Shutko";
+
+for (let i = 1; i < 6; i++) {
+    if (i % 2 == 0) {
+        console.log("Привет " + MyName.toLowerCase());
+    } else {
+        console.log("Привет " + MyName.toUpperCase());
+    }
+}
+console.log("");
+
+//task2
+console.log("task 2");
+
+const myStr = "я учу typescript!";
+const myWord = "я";
+if (myStr.includes(myWord)) {
+    console.log(myStr.indexOf("учу"));
+} else {
+    console.log("Данной подстроки нет");
+}
+console.log("");
+
+//task3
+console.log("task 3");
+
+const str = "Дана произвольная строка";
+const symbolPos = 22;
+if (symbolPos > str.length) {
+    console.log("Вы вышли за границу строки");
+} else {
+    console.log("символ номер " + symbolPos + " это символ " + str.charAt(symbolPos));
+}
+console.log("");
+
+////task4
+console.log("task 4");
+
+const str1 = "Дана произвольная строка.";
+if (str1.endsWith(".")) {
+    console.log("Данное предложение закончено");
+} else {
+    console.log("В конце предложения не хватает точки");
+}
+console.log("");
+
+////task5
+console.log("task 5");
+//беру эту строку из таска 2
+console.log(myStr.slice(2, 5));
+console.log(myStr.substring(6, 16));
+console.log("");
+
+////task6
+console.log("task 6");
+//беру эту строку из таска 2
+console.log(myStr.split(" "));
+console.log("");
+
+////task7
+console.log("task 7");
+const myStr1 = "                я учу typescript!                ";
+console.log(myStr1.trim());
+console.log("");
+
+////task8
+console.log("task 8");
+const num = 8.829734872948;
+console.log(num.toFixed(3));
+console.log("");
+
+////task9
+console.log("task 9");
+
+const myNum = 12856.415451539999;
+
+if (Number.isInteger(myNum)) {
+    console.log(myNum);
+} else {
+    console.log(Math.round(myNum));
+}
+console.log("");
+
+////task10
+console.log("task 10");
+const myNum1 = 666.9999;
+console.log(Math.floor(myNum1));
+console.log(Math.ceil(myNum1));
+console.log(Math.round(myNum1));
+console.log("");
+
+////task11
+console.log("task 11");
+const myNum2 = 29;
+console.log(Math.pow(myNum2, 2));
+console.log("");
+
+////task12
+console.log("task 12");
+const min = 333;
+const max = 666;
+const res = Math.floor(Math.random() * (max - min) + min);
+console.log(res);
+console.log("");
+
+////task13
+console.log("task 13");
+//беру аргументы фунции из таска 12
+const res1 = Math.random() * (max - min) + min;
+console.log(res1);
+console.log("");
+
+////task14
+console.log("task 14");
+const date: Date = new Date();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const day = date.getDate();
+const date0 = month + "/" + year + "/" + day;
+
+console.log("текущая дата:  " + date0 + ". Текущее время " + date.toLocaleTimeString());
